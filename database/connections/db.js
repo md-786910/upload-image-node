35431a26aa2f5d9199342e44fa7fc266ae2e3c38
md ashif reza <mdashifreza111@gmail.com>
2021-09-07 09:32:40 +0530

    add mongodb atlas

--- database/connections/db.js
+++ database/connections/db.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://localhost:27017/imageUpload", {
+//mongodb+srv://db:6O3rHBpJYYLnGjbV@database.l2fnk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+// ', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// mongodb://localhost:27017/imageUpload
+
+mongoose.connect("mongodb+srv://db:6O3rHBpJYYLnGjbV@database.l2fnk.mongodb.net/imageUpload?retryWrites=true&w=majority", {
+    keepAlive: true,
+    useNewUrlParser: true,
     useUnifiedTopology: true,
-    useNewUrlParser: true
+    // useFindAndModify: false,
+
 }).then(() => {
     console.log("connect")
 }).catch(err => {
     console.log(err)
 })
 
-
-// module.exports = db
